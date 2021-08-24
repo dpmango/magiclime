44f38a1fa47d19d03a66c6e5c05fdf25b16bb538
Sergey Khmelevskoy <xs290@me.com>
2021-08-24 19:28:44 +0300

    #57 Add onlyNumbers and masked inputs

--- src/components/Common/Controls/Formik/Input/index.tsx
+++ src/components/Common/Controls/Formik/Input/index.tsx
@@ -1,17 +1,24 @@
+/* eslint-disable @typescript-eslint/no-shadow */
 import React, { FC, SyntheticEvent, useCallback } from 'react';
 import { TextField, TextFieldProps } from '@consta/uikit/TextField';
 import { Field, FieldHookConfig, FieldProps } from 'formik';
-import Typography from '../../../Typography';
+import InputMask from 'react-input-mask';
+
+import Typography from 'components/Common/Typography';
+import Flex from 'components/Common/Flex';
+import { getNestedValue } from 'utils/formik/getNestedValue';
+import { ChangeType } from 'types/common';
 import PasswordInput from '../../PasswordInput';
-import Flex from '../../../Flex';
 import MemoWrapper from '../MemoWrapper';
+
 import useStyles from './styles';
-import { getNestedValue } from '../../../../../utils/formik/getNestedValue';
 
 interface IProps extends TextFieldProps {
   label?: string;
+  mask?: string | null;
   isRequired?: boolean;
   isPassword?: boolean;
+  onlyNumbers?: boolean;
 }
 
 const FormikInput = (props: IProps) => {
@@ -23,15 +30,28 @@ const FormikInputComponent = MemoWrapper(
     field: { value, onChange, ...field },
     form: { setFieldValue, errors, touched },
     label,
+    mask = null,
     isRequired = true,
     isPassword = false,
+    onlyNumbers = false,
     ...props
   }: IProps & FieldProps) => {
     const styles = useStyles();
 
     const handleChange = useCallback(({ value }: { value: string | null }) => {
+      console.log('onchange event');
       setFieldValue(field.name, value || '');
     }, []);
+
+    const handleKeyDown = (event: ChangeType) => {
+      if (onlyNumbers) {
+        const isNumber = !Number.isNaN(parseFloat(event.key));
+        const isAllowedCharacter = [8, 13, 32].includes(event.keyCode); // backspace, enter, space
+        if (!isNumber && !isAllowedCharacter) {
+          event.preventDefault();
+        }
+      }
+    };
 
     const fieldError = getNestedValue(errors, field.name);
     const fieldTouched = getNestedValue(touched, field.name);
@@ -44,25 +64,47 @@ const FormikInputComponent = MemoWrapper(
             {isRequired && <span className={styles.star}>*</span>}
           </Typography>
         )}
-        {!isPassword ? (
-          <TextField
-            className={styles.input}
-            value={value}
-            state={fieldError && fieldTouched ? 'alert' : undefined}
-            onChange={handleChange}
-            {...field}
-            {...props}
-          />
-        ) : (
+
+        {isPassword && (
           <PasswordInput
             className={styles.input}
+            state={fieldError && fieldTouched ? 'alert' : undefined}
             value={value}
             onChange={handleChange}
-            state={fieldError && fieldTouched ? 'alert' : undefined}
             {...field}
             {...props}
           />
         )}
+
+        {mask ? (
+          <InputMask
+            mask={mask}
+            value={value}
+            onChange={handleChange}
+            onKeyDown={handleKeyDown}
+            {...field}
+            {...props}
+          >
+            {(inputProps) => {
+              <TextField
+                className={styles.input}
+                state={fieldError && fieldTouched ? 'alert' : undefined}
+                {...inputProps}
+              />;
+            }}
+          </InputMask>
+        ) : (
+          <TextField
+            className={styles.input}
+            state={fieldError && fieldTouched ? 'alert' : undefined}
+            value={value}
+            onChange={handleChange}
+            onKeyDown={handleKeyDown}
+            {...field}
+            {...props}
+          />
+        )}
+
         {fieldError && fieldTouched && (
           <Typography margin="5px 0 0" size="xs" view="alert">
             {fieldError}
