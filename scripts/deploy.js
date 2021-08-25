e61c13fad0f516cc76f432534cf742ad02962fa4
Incold0310 <130466@rambler.ru>
2021-08-25 14:29:03 +0300

    Update README.md file

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,18 +2,34 @@
 const Client = require('ssh2-sftp-client');
 const path = require('path');
 const sftp = new Client();
+const fs = require('fs');
 
-const config = {
+if (
+  !process.argv[2] ||
+  !(process.argv[2] === 'prod' || process.argv[2] === 'dev')
+) {
+  console.log('Неверный параметр! Доступные параметры: dev, prod');
+  process.exit(1);
+}
+
+const prodConfig = {
   host: '185.100.87.66',
   port: '65222',
   username: 'root',
   password: 'GwhrWHRweHYK35yhreE',
 };
 
+const devConfig = {
+  host: '178.154.196.41',
+  port: '443',
+  username: 'dev',
+  privateKey: fs.readFileSync('~/.ssh/id_rsa.pub'),
+};
+
 const REMOTE_PATH = '/var/www/html';
 
 sftp
-  .connect(config)
+  .connect(process.argv[2] === 'prod' ? prodConfig : devConfig)
   .then(() => {
     return sftp.list(REMOTE_PATH);
   })
